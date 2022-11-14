e570143ee506f45dfb54cf85e93ae98963527ad7
aymenmzz <105977772+aymenmzz@users.noreply.github.com>
2022-11-14 07:48:08 -0800

    Update CustomLink.js

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function CustomLink({ href, children, style }) {
-  const optionToast = { autoClose: 1900 };
+  const optionToast = { autoClose: 1400 };
   const notifyLoading = () =>
     toast.info(
       "Veuillez patienter, le chargement de la page est en cours...",
