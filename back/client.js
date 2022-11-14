06d58070a747d390c6ac1479744e437dc55cb22f
aymenmzz <105977772+aymenmzz@users.noreply.github.com>
2022-11-14 07:45:58 -0800

    Using environment variable to reach mongoDB

--- back/client.js
+++ back/client.js
@@ -1,4 +1,2 @@
 const { MongoClient } = require("mongodb");
-export const client = new MongoClient(
-  "mongodb+srv://amaazou:14Predator@cluster1.8r9wgxi.mongodb.net/?retryWrites=true&w=majority"
-);
+export const client = new MongoClient(process.env.MONGO_URI);
